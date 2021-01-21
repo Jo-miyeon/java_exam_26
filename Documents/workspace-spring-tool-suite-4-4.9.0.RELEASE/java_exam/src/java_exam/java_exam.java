e678d17ca44d7288b4a8e0693636b7be8f79064e
Jo-miyeon <my0v0@naver.com>
2021-01-21 19:40:32 +0900

    게시판만들기 삭제기능완료

--- Documents/workspace-spring-tool-suite-4-4.9.0.RELEASE/java_exam/src/java_exam/java_exam.java
+++ Documents/workspace-spring-tool-suite-4-4.9.0.RELEASE/java_exam/src/java_exam/java_exam.java
@@ -1,66 +1,67 @@
 package java_exam;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class java_exam {
 
 	public static void main(String[] args) {
-		
-		VendingMachine vm = new VendingMachine();
-		
-		vm.printAllBeverages();
-		vm.setinputedMoney(2000);
-		
-    // 출력 : 0.콜라 : 1000
-    // 출력 : 1.사이다 : 1200
-    // 출력 : 2.커피 : 800
+		ArrayList<String> titles = new ArrayList<>();
+		ArrayList<String> bodies = new ArrayList<>();
+		ArrayList<Integer> ids = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		int no = 1;
 
-		vm.setinputedMoney(2000);
-    // 출력 : 현재 투입된 금액은 2000원입니다.
+		while (true) {
 
-		String rst = vm.getBeverage(1);
-    // 출력 : 사이다를 뽑으셨습니다.
-    // 출력 : 잔액은 800원입니다.
-
-		System.out.println(rst);
-    // 출력 : 사이다
-
-    String rst2 = vm.getBeverage(0);
-    // 출력 : 잔액이 부족합니다.
-
-		vm.setinputedMoney(500);
-    // 출력 : 현재 투입된 금액은 1300원입니다.
-		
-    String rst3 = vm.getBeverage(0);
-    // 출력 : 콜라를 뽑으셨습니다.
-    // 출력 : 잔액은 300원입니다.
-		System.out.println(rst3);
-    // 출력 : 콜라
-	}
-}
-class VendingMachine {
-	int inputedMoney = 0;
-	int change = 0;
-	String beverageNames[] = {"콜라","사이다","커피"};
-	int beveragePrices[] = {1000,1200,800};
-	//금액 투입기능  
-	void setinputedMoney(int money) {
-		inputedMoney = inputedMoney+money;
-		System.out.println("현재투입된 금액은"+inputedMoney+"입니다.");
-	}
-	void printAllBeverages() {
-		for(int i=0; i<beverageNames.length; i++) {
-			System.out.println(i+"."+beverageNames[i]+":"+beveragePrices[i]);
+			System.out.print("명령어입력 : ");
+			String str = sc.nextLine();
+			if (str.equals("exit")) {
+				break;
+			} else if (str.equals("add")) {
+				ids.add(no);
+				no++;
+				System.out.println("게시물 제목을 입력해주세요 :");
+				String title = sc.nextLine();
+				titles.add(title);
+				System.out.println("게시물 내용을 입력해주세요 :");
+				String body = sc.nextLine();
+				bodies.add(body);
+				System.out.println("게시물이 등록되었습니다.");
+			} else if (str.equals("list")) {
+				for (int i = 0; i < titles.size(); i++) {
+					System.out.println("게시물 번호:" + ids.get(i));
+					System.out.println("제목 :" + titles.get(i));
+					System.out.println("내용 :" + bodies.get(i));
+					System.out.println("======================");
+				}
+			} else if (str.equals("update")) {
+				System.out.println("수정할 게시물의 번호를 선택해주세요 :");
+				int targetId = Integer.parseInt(sc.nextLine());
+				
+				for (int i = 0; i < ids.size(); i++) {
+					if (ids.get(i) == targetId) {
+						System.out.println("수정할 게시물의 제목을 입력해주세요 :");
+						String newTitle = sc.nextLine();
+						System.out.println("수정할 게시물의 내용을 입력해주세요 :");
+						String newBody = sc.nextLine();
+						titles.set(i, newTitle);
+						bodies.set(i, newBody);
+						System.out.println("게시물이 수정되었습니다.");
+					}
+				}
+			}else if(str.equals("delete")) {
+				System.out.println("삭제할 게시물의 번호를 선택해주세요 :");
+				int targetId = Integer.parseInt(sc.nextLine());
+				for(int i=0;i<ids.size();i++) {
+					if(ids.get(i)==targetId) {
+						titles.remove(i);
+						bodies.remove(i);
+					}
+				}
+				System.out.println("게시물이 삭제되었습니다.");
+			}
 		}
 	}
-	String getBeverage(int bno) {
-		if(inputedMoney < beveragePrices[bno]) {
-			System.out.println("잔액이 부족합니다.");
-			return "";
-		}
-		inputedMoney = inputedMoney - beveragePrices[bno]; //거스름돈 
-		System.out.println("잔액은"+inputedMoney+"입니다.");
-		return beverageNames[bno];
-	}
+
 }
-
-
-
